1fd9bcc470f022423187db21fb2c9f0eda1bf96c
Saketh <sakethramsettipalli2003@gmal.com>
2025-03-09 16:34:53 +0000

    few more changes

--- 2.py
+++ 2.py
@@ -2,6 +2,6 @@ from math import gcd
 def gcdOfStrings(word1,word2) :
     result=''  
     if word1+word2==word2+word1 :
-        t = gcd (len(word1),len(word2)) 
+        t = gcd (len(word1),len(word1)) 
         return word1[0:t]
     return ""
